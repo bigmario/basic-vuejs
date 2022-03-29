440f031954b6f1f12595fbcd21ef5a406b6fefd2
Mario Castro <mariocastro.pva@gmail.com>
2022-03-29 10:20:31 -0400

    using vue create

---
+++ hola/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>{{ text }}</h1>
+  <p>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad mollitia minima
+    at quia a, earum eum odit blanditiis nam qui nobis incidunt sed, magni
+    corporis, dolorum numquam molestias porro vero.
+  </p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: "Hola Vue",
+    };
+  },
+};
+</script>
+<style>
+p {
+  color: red;
+}
+</style>
